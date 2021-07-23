d193b3e738f2563fec70b419ec08966936dd06be
pomkos <albeithome@gmail.com>
2021-07-23 16:15:50 -0400

    Instructions, prompt user to add conda loc

--- start_me.bat
+++ start_me.bat
@@ -4,7 +4,21 @@
 
 :: EDIT ME ::
 :: Edit the below line to point to the anaconda directory as demonstrated below ::
-CALL C:\Users\albei\anaconda3\Scripts\activate.bat C:\Users\albei\anaconda3
+
+if exists C:\Users\albei\miniconda3\ (
+    loc = C:\Users\albei\miniconda3
+) else (
+    echo "Select the correct location, then replace the old one in start_me.bat with the new one" >"conda_location.txt"
+    echo where conda >"conda_location.txt"
+    echo "Please edit the start_me.bat file using the correct location from conda_location.txt"
+    EXIT /B
+)
+
+echo "Activating miniconda"
+
+CALL loc\Scripts\activate.bat loc
+
+echo "Starting the dynamic bike script"
 
 :: This line just runs the streamlit script ::
 streamlit run new_bike_st.py
