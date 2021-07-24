edc3ef910d9052b3b08ecc49c880aab56a6b4ad0
Peter Gates <albeithome@gmail.com>
2021-07-24 12:43:39 -0400

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 :: This file installs or reinstalls the required python libraries for dynamic bike script to run
-:: Assumes miniconda has been installed already
-:: To install miniconda: https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe
+:: Last edited by:
+::                 Peter Gates; July 24, 2021 ::
 
 @ECHO OFF
 
